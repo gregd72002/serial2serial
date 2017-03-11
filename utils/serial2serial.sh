aa5a3cd9ef8bde4abaac3f16e721a783c7e0e402
Gregory <gregd72002@gmail.com>
2017-03-11 15:00:51 +0000

    Startup script

--- utils/serial2serial.sh
+++ utils/serial2serial.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 while [ 1 ]; do
     sleep 1;
-    if [ -c $1 ] && [ -c $2 ]; then 
+    if [ -c $1 ] && [ -c $2 ] && [ $# -eq 2 ]; then 
 	/usr/local/bin/serial2serial -a $1 -b $2
+    elif [ -c $1 ] && [ -c $2 ] && [ $# -eq 3 ]; then 
+	/usr/local/bin/serial2serial -a $1 -b $2 -s $3
+    else
+	echo "Incorrect parameters";
+	exit 1;
     fi
 done
 
